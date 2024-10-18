8f4a85e0e314bc8f74750045116531fb3c99f6fc
Dealy Alti <dealyalti_2226240037@mhs.mdp.ac.id>
2024-10-18 18:33:06 +0700

    Building a data model with MongoDB and Mongoose

--- my-express-mvc/app.js
+++ my-express-mvc/app.js
@@ -3,10 +3,11 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-require('./app_server/models/d');
+require('./app_server/models/db');
 
 var indexRouter = require('./app_server/routes/index');
 var usersRouter = require('./app_server/routes/users');
+var mahasiswaRouter = require('./app_server/routes/mahasiswas');
 
 var app = express();
 
@@ -22,6 +23,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/api', mahasiswaRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
