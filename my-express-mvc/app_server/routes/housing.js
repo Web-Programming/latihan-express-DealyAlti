5f6944dcfe1c508a0547ab88d4d80ada65170e20
Cesya <dealyalti_2226240037@mhs.mdp.ac.id>
2024-11-20 22:53:29 +0700

    API Housing

--- my-express-mvc/app_server/routes/housing.js
+++ my-express-mvc/app_server/routes/housing.js
@@ -2,5 +2,5 @@ const express = require("express");
 const router = express.Router();
 const housingController = require("../controllers/controllerHousing");
 // Fungsi dan Rute Index Kita Gunakan Untuk Memanggil Semua Data Dalam Database MongoDB
-router.get("/housing", housingController.Index);
+router.get("/", housingController.Index);
 module.exports = router;
